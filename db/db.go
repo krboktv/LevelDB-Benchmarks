88d63b0b8a1ff97a3c03508658756d372a88d04d
Kirill <raptor@MacBook-Pro-Kirill.local>
2018-09-22 14:45:43 +0300

    Refactor

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"../file"
+	"fmt"
 	"github.com/syndtr/goleveldb/leveldb"
 	"log"
 	"os"
@@ -25,49 +26,99 @@ func Connect() (*leveldb.DB)  {
 }
 
 func Get(db *leveldb.DB, key []string, count int) {
-	var i = 0
-	timer1 := time.NewTimer(time.Duration(nanoseconds))
-	for ; i < count; i++ {
-		db.Get([]byte(key[i]), nil)
-	}
-	<-timer1.C
-	println("Get query (query per second)")
-	qps := i / seconds
-	println(qps)
-	file.Write("get.txt", strconv.Itoa(qps))
-	if err != nil {
-		log.Fatal(err)
+	var i= 0
+
+	endSignal := make(chan bool, 1)
+	go sleep(seconds, endSignal)
+
+	pollInt := time.Second
+
+	go func() {
+		for ; i < count; i++ {
+			db.Get([]byte(key[i]), nil)
+		}
+	}()
+
+	for {
+		select {
+		case <-endSignal:
+			println("Get query (query per second)")
+			qps := i / seconds
+			println(qps)
+			file.Write("get.txt", strconv.Itoa(qps))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("The end!")
+			return
+		}
+		time.Sleep(pollInt)
 	}
 }
 
 func Put(db *leveldb.DB, key []string, value []string, count int) {
-	var i = 0
-	timer1 := time.NewTimer(time.Duration(nanoseconds))
-	for ; i < count; i++ {
-		db.Put([]byte(key[i]), []byte(value[i]), nil)
-	}
-	<-timer1.C
-	println("Put query (query per second)")
-	qps := i / seconds
-	println(qps)
-	file.Write("put.txt", strconv.Itoa(qps))
-	if err != nil {
-		log.Fatal(err)
+	var i= 0
+
+	endSignal := make(chan bool, 1)
+	go sleep(seconds, endSignal)
+
+	pollInt := time.Second
+
+	go func() {
+		for ; i < count; i++ {
+			db.Put([]byte(key[i]), []byte(value[i]), nil)
+		}
+	}()
+
+	for {
+		select {
+		case <-endSignal:
+			println("Put query (query per second)")
+			qps := i / seconds
+			println(qps)
+			file.Write("put.txt", strconv.Itoa(qps))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("The end!")
+			return
+		}
+		time.Sleep(pollInt)
 	}
 }
 
 func Delete(db *leveldb.DB, key []string, count int) {
-	var i = 0
-	timer1 := time.NewTimer(time.Duration(nanoseconds))
-	for ; i < count; i++ {
-		db.Delete([]byte(key[i]), nil)
+	var i= 0
+
+	endSignal := make(chan bool, 1)
+	go sleep(seconds, endSignal)
+
+	pollInt := time.Microsecond
+
+	go func() {
+		for ; i < count; i++ {
+			db.Delete([]byte(key[i]), nil)
+		}
+	}()
+
+	for {
+		select {
+		case <-endSignal:
+			println("Delete query (query per second)")
+			qps := i / seconds
+			println(qps)
+			file.Write("delete.txt", strconv.Itoa(qps))
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println("The end!")
+			return
+		}
+		time.Sleep(pollInt)
 	}
-	<-timer1.C
-	println("Delete query (query per second)")
-	qps := i / seconds
-	println(qps)
-	file.Write("delete.txt", strconv.Itoa(qps))
-	if err != nil {
-		log.Fatal(err)
-	}
+}
+
+func sleep(seconds int, endSignal chan<- bool) {
+	time.Sleep(time.Duration(seconds) * time.Second)
+	endSignal <- true
 }
--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"./db"
-	"os"
-	"strconv"
 	"./file"
 	"strings"
 )
@@ -11,19 +9,15 @@ import (
 func main() {
 	db.Connect()
 
-	var env, err = strconv.Atoi(os.Getenv("SECONDS"))
-	arraysLength := env * 1000000
-	file.CreateKeyValues(arraysLength, 32, 100)
+	//var env, err = strconv.Atoi(os.Getenv("SECONDS"))
+	arraysLength := 2000000000
+	//file.CreateKeyValues(arraysLength, 32, 100)
 
 	d1 := file.Read("keys.txt")
 	d2 := file.Read("values.txt")
 
 	keys := strings.Split(d1, ",")
 	values := strings.Split(d2, ",")
-
-	if err != nil {
-		panic(err.Error())
-	}
 
 	db.Put(db.Connect(), keys, values, arraysLength)
 
